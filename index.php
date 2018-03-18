e8921d425b7134af8e752e1f1654264a2b38902b
Kapil Kataria <kapil.kataria@srijan.net>
2018-03-19 00:01:09 +0530

    JIRA

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
  * The PHP page that serves all page requests on a Drupal installation.
  *
  * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" direct
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
 
-use Drupal\Core\DrupalKernel
+use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
 $autoloader = require_once 'autoload.php';
